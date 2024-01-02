2b98160a6589beb4bf6efb3973684cd6886bbf51
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-02 14:40:57 -0800

    .

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for c in range(90, 64, -1):
-    print("{:c}".format(c), end="")
-    if c % 2 == 0:
-        c += 32
-print()
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
