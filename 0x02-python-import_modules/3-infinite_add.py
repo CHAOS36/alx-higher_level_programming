fee53356b85e33934ed8c0197bb7d99d7e156e21
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-04 13:45:42 -0800

    .

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    
+    import sys
+
+    tol = 0
+    for i in range(len(sys.argv) - 1):
+        tol += int(sys.argv[i + 1])
+    print("{}".format(tol))
