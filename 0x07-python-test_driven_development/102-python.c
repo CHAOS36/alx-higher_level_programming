06b47e6f99ace96acf86e2b0195343e5acf574f4
badr <badrelhoudni12@gmail.com>
2024-01-31 00:10:35 +0100

    .

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,28 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int pipox;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	pipox = ((PyASCIIObject *)(p))->pipox;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", pipox);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &pipox));
+}
